dd380806e9125eb7f1aa471ee250b2ffe5f88776
FabianZ1990 <fabian.zischler@gmail.com>
2023-01-24 16:43:15 +0000

    added String array

--- src/Outline.java
+++ src/Outline.java
@@ -3,7 +3,10 @@ import java.util.Arrays;
 public class Outline {
   public static void main(String... args) { // varargs alternative to String[]
     Integer[] intArray = {1,7,3,4,8,2};
+    String[]  stringArray = {"apple", "kiwi", "passionfruit", "banana", "peach", "apricot", "pineapple" };
+
     System.out.println(Arrays.asList(intArray));
+    System.out.println(Arrays.asList(stringArray));
     // Arrays.sort(intArray,.......)
   } 
 }
