66c15ae9c801705fe823d8f4b71c3c23ccd5015c
FabianZ1990 <fabian.zischler@gmail.com>
2023-01-26 18:25:15 +0000

    before lab2

--- src/Outline.java
+++ src/Outline.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 public class Outline {
   public static void main(String... args) { // varargs alternative to String[]
     Integer[] intArray = {1,7,3,4,8,2};
-    String[]  stringArray = {"apple", "kiwi", "passionfruit", "banana", "peach", "apricot", "pineapple" };
+    String[]  stringArray = {"apple", "kiwi", "passionfruit", "eggfruit", "banana", "peach", "apricot", "pineapple", "elderberry" };
 
     System.out.println(Arrays.asList(intArray));
     System.out.println(Arrays.asList(stringArray));
